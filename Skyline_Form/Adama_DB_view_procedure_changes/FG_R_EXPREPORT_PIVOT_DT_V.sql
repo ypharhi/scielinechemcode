b4b1523591006b0108427482d0c8d308a344fd86
yaron.pharhi <yaron.pharhi@comply.co.il>
2021-07-18 21:47:55 +0300

    update view - develop experiment report

--- Skyline_Form/Adama_DB_view_procedure_changes/FG_R_EXPREPORT_PIVOT_DT_V.sql
+++ Skyline_Form/Adama_DB_view_procedure_changes/FG_R_EXPREPORT_PIVOT_DT_V.sql
@@ -1,7 +1,10 @@
 CREATE OR REPLACE VIEW FG_R_EXPREPORT_PIVOT_DT_V AS
-select t.experiment_id,--important: every change of the column names here or in the fg_p_experimentanalysis_v should be taken in account in the design development and in the js func' getWidthByNameForExpAnalysisReport
-       '{"displayName":"' || t.ExperimentName || '" ,"icon":"' || '' || '" ,"fileId":"' || '' || '","formCode":"' || t.FORMCODE || '"  ,"formId":"' || t.EXPERIMENT_ID || '","tab":"' || '' || '" }' as "Experiment Number_SMARTLINK",
-       t.DESCRIPTION as "Experiment Description"
+select distinct t.experiment_id,--important: every change of the column names here or in the fg_p_experimentanalysis_v should be taken in account in the design development and in the js func' getWidthByNameForExpAnalysisReport
+       t.SAMPLE_ID,
+       t.UNIQUEROW,
+       --t.SAMPLE_ID || '_' || t.experiment_id as uniquerow,
+       '{"displayName":"' || e.ExperimentName || '" ,"icon":"' || '' || '" ,"fileId":"' || '' || '","formCode":"' || t.FORMCODE || '"  ,"formId":"' || t.EXPERIMENT_ID || '","tab":"' || '' || '" }' as "Experiment Number_SMARTLINK",
+       e.DESCRIPTION as "Experiment Description"
         /*t.experimentstatusname as "Experiment Status",
        fg_get_richtext_display(t.AIM) as "Experiment Aim",
        fg_get_richtext_display(t.CONCLUSSION) as "Experiment Conclusions",
@@ -11,5 +14,9 @@ select t.experiment_id,--important: every change of the column names here or in 
        nvl2(t.quantity,fg_get_uom_display(t.quantity_uom),'') as "Quantity UOM",
        t.moles as "Moles",
        nvl2(t.moles,fg_get_uom_display(t.moleuom_id),'') as "Moles UOM",*/
-       ,'SELECT result_SMARTPIVOT FROM FG_P_EXPREPORT_V' AS RESULT_SMARTPIVOTSQL
-from fg_s_experiment_v t;
+       --,'SELECT result_SMARTPIVOT FROM FG_P_EXPREPORT_V' AS RESULT_SMARTPIVOTSQL
+from /*fg_s_experiment_v t,
+     FG_I_RESULT_ALL_V r*/
+    fg_r_experimentresult_noreq_v t, fg_s_experiment_v e  
+where t.experiment_id = e.experiment_id 
+order by t.experiment_id, t.SAMPLE_ID
