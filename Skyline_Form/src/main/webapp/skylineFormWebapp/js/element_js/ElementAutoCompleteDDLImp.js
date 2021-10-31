973a7bdfdee3fd9d4ec1f16ff72dc0d6f2c72650
Adi Biton <adi.biton1@ness-tech.co.il>
2021-10-31 14:55:54 +0200

    fixed bug 9392

--- Skyline_Form/src/main/webapp/skylineFormWebapp/js/element_js/ElementAutoCompleteDDLImp.js
+++ Skyline_Form/src/main/webapp/skylineFormWebapp/js/element_js/ElementAutoCompleteDDLImp.js
@@ -43,7 +43,7 @@ var ElementAutoCompleteDDLImp = {
 
 function add_arrowDDL(domId){
 	var arrowElemeToAdd = '<div style="position: absolute;top: 0;right: 0;display: block; width: 18px;height: 100%;">'
-    +'<b class="arrows-search" style="height:100%;background: url(http://localhost:9080/Adama/skylineFormWebapp/js/chosen-sprite.png) no-repeat 0 5px;"></b>'
+    +'<b class="arrows-search" style="height:100%;background: url(../skylineFormWebapp/js/chosen-sprite.png) no-repeat 0 5px;"></b>'
     +'</div>';
 	var $chosenElement = $('#'+domId +"_chosen");
 	$chosenElement.find('ul.chosen-choices').first().after(arrowElemeToAdd);
